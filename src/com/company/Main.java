fd5c0ba4a361acf07d7c3954bcdf60f5ee7cc517
Đăng Minh Đức Đỗ <minhduc100298@gmail.com>
2021-04-04 21:34:45 +0700

    update file

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Mass mass = new Mass();
+
+        //Test
+        //
+        ArrayList<Mass.ElementAnalyse> arr = mass.compressMolecule("CH3COONa");
+//        for (Mass.ElementAnalyse ea : arr) {
+//            System.out.println("Element is " + ea.Element + " valence is " + ea.Valence);
+//        }
+
+    }
+}
